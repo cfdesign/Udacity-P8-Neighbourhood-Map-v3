bf79c3f701b25828b0d173579ff6bf8b3a8f40b1
Chris Faulds <fauldzy@hotmail.co.uk>
2018-09-12 10:58:44 +0100

    marker animation + infowindow to marker

--- src/components/map.js
+++ src/components/map.js
@@ -3,21 +3,21 @@ import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 import MapMarker from "./marker";
 
 const Map = withScriptjs(withGoogleMap((props) => {
-  console.log(props.locations+" from map")
-  return (
-      <GoogleMap
-        defaultZoom={16}
-        center={ { lat:  51.377770, lng: 0.100793} }
-        >
-        {props.locations.map( location =>
-            <MapMarker
-                key={location.id}
-                location={location}
-                position={{lat: location.location.lat, lng: location.location.lng}}
-            />)}
-      </GoogleMap>
-    );
-  }
+    console.log(props.locations+" from map")
+    return (
+        <GoogleMap
+            defaultZoom={16}
+            center={ { lat:  51.377770, lng: 0.100793} }
+            >
+            {props.locations.map( location =>
+                <MapMarker
+                    key={location.id}
+                    location={location}
+                    position={{lat: location.location.lat, lng: location.location.lng}}
+                />)}
+        </GoogleMap>
+        );
+    }
 ))
 
 export default Map;
--- src/components/marker.js
+++ src/components/marker.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { Marker } from "react-google-maps";
+import { Marker, InfoWindow } from "react-google-maps";
 
 class MapMarker extends Component {
+    state = {
+        isOpen: false
+    }
+    onToggleOpen  = () => {
+		this.setState({isOpen: !this.state.isOpen})
+	}
 
-  render(){
-    return(
-        <Marker
-            title={this.props.location.name}
-            position={this.props.position}
-        >
-        </Marker>
-    );
-  }
+    render(){
+        return(
+            <Marker
+                title={this.props.location.name}
+                position={this.props.position}
+                animation={window.google.maps.Animation.DROP}
+                onClick={this.onToggleOpen}
+            >
+            {this.state.isOpen &&
+                <InfoWindow onCloseClick={this.onToggleOpen}>
+                    <div>
+                        <div>{this.props.location.name}</div>
+                        <div>{this.props.location.categories[0].name}</div>
+                    </div>
+                </InfoWindow>
+            }
+            </Marker>
+        );
+    }
 }
 export default MapMarker
