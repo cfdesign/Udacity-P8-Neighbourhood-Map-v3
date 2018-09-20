4039778734186791c623e18e5661233fef17fcb8
Chris Faulds <fauldzy@hotmail.co.uk>
2018-09-20 17:38:36 +0100

    Simplified marker func logic

--- src/components/marker.js
+++ src/components/marker.js
@@ -7,17 +7,15 @@ class MapMarker extends Component {
     }
 
 	componentDidUpdate(prevProps) {
-        if (this.props.listId !== prevProps.listId) {
+        if ((this.props.listId !== prevProps.listId) && (this.props.location.id === this.props.listId)) {
             // This function can also conversely add markers, it serves to both add and remove.
-            this.open()
+            this.interActions()
 		}
 	}
 
-    open() {
-        if (this.props.location.id === this.props.listId) {
-            this.props.markerClicked(this.props.location)
-            this.animate()
-        }
+    interActions() {
+        this.props.markerClicked(this.props.location)
+        this.animate()
         //this.props.location.id === this.props.listId ?
         //this.props.markerClicked(this.props.location) : this.setState({animation: 2})
     }
@@ -38,7 +36,7 @@ class MapMarker extends Component {
                 //animation={window.google.maps.Animation.DROP}
                 //icon={{anchor: new window.google.maps.Point(17, 34)}}
                 animation= {this.state.animation}
-                onClick={() => {this.props.markerClicked(this.props.location); this.animate()}}
+                onClick={() => this.interActions()}
             />
         );
     }
